aa6e02e4aef387cd566b603659ca0d7c6a122ca0
Alexander Solovyov <alexander@solovyov.net>
2019-07-01 11:33:49 +0300

    1.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject modnakasta/bigq "1.1.1"
+(defproject modnakasta/bigq "1.1.2"
   :description "Library for making requests to BigQuery"
   :url "https://github.com/kasta-ua/bigq"
   :license {:name "Eclipse Public License - v 1.0"
@@ -7,8 +7,8 @@
             :comments "same as Clojure"}
 
   :dependencies
-  [[org.clojure/clojure "1.10.0"]
-   [metosin/jsonista "0.2.2"]
-   [clj-http "3.9.1"]
-   [buddy/buddy-sign "3.0.0"]
+  [[org.clojure/clojure "1.10.1" :scope "provided"]
+   [metosin/jsonista "0.2.3"]
+   [clj-http "3.10.0"]
+   [buddy/buddy-sign "3.1.0"]
    [com.taoensso/timbre "4.10.0"]])
